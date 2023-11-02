780c91a994d41a51c3185785dbab53f61a155333
JAINISH <jainishthakor6841@gmail.com>
2023-11-02 23:03:20 +0530

    update

--- app/src/main/java/com/example/mad_practical_6_21012021113/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_6_21012021113/MainActivity.kt
@@ -3,32 +3,36 @@ package com.example.mad_practical_6_21012021113
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val browsebutton: Button = findViewById(R.id.button)
-        val browsebutton: Button = findViewById(R.id.button2)
 
-        browsebutton.setOnClickListener {
-            playpause()
+        val Play:FloatingActionButton=findViewById(R.id.play_btn)
+
+        Play.setOnClickListener {
+            PlayPause()
         }
-        browsebutton.setOnClickListener {
-            stop()
+        val Pause : FloatingActionButton=findViewById(R.id.stop_btn)
+
+        Pause.setOnClickListener {
+            Stop()
         }
     }
 
-    fun playpause(){
-        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply {
+    fun PlayPause(){
+        Intent(applicationContext, MyService::class.java).putExtra(MyService.PLAYERKEY, MyService.PLAYERVALUE).apply {
             startService(this)
         }
-    }
-    fun stop(){
-        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,MyService.PLAYERVALUE).apply {
-            startService(this)
-        }
+
     }
 
+    fun Stop(){
+        Intent(applicationContext, MyService::class.java).putExtra(MyService.PLAYERKEY, MyService.PLAYERVALUE).apply {
+            stopService(this)
+        }
+
+    }
 }
--- app/src/main/java/com/example/mad_practical_6_21012021113/MyService.kt
+++ app/src/main/java/com/example/mad_practical_6_21012021113/MyService.kt
@@ -6,23 +6,24 @@ import android.media.MediaPlayer
 import android.os.IBinder
 
 class MyService : Service() {
-    lateinit var player:MediaPlayer
+    lateinit var player: MediaPlayer
     companion object{
-        val PLAYERKEY="service"
-        val PLAYERVALUE="play/pause"
+        val PLAYERKEY="Service"
+        val PLAYERVALUE="play/Pause"
     }
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (!this::player.isInitialized){
-            player = MediaPlayer.create(this,R.raw.song)
+        if(!this::player.isInitialized){
+            player= MediaPlayer.create(this, R.raw.song)
         }
-        if (intent != null){
-            val datavalue=intent.getStringExtra(PLAYERKEY)
-            if (datavalue== PLAYERVALUE){
-                if (!player.isPlaying){
+        if(intent!=null){
+            val dataValue=intent.getStringExtra(PLAYERKEY)
+            if (dataValue== PLAYERVALUE){
+                if(!player.isPlaying){
                     player.start()
                 }
                 else{
